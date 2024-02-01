72af3ff7c0b57ffce3048254b839e7f5cd95696a
dkiriinya <dkiriinya06sc@gmail.com>
2024-02-01 14:51:30 +0300

    learnt how to propagate up using callbacks

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  function handeClick() {
+    const newColor = getRandomColor()
+    onChangeColor(newColor)
+  }
+  console.log(onChangeColor)
+  return <div onClick={handeClick} className="child" style={{ backgroundColor: color }} />;
 }
 
 export default Child;
